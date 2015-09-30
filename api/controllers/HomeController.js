0310a46c6832a8d84ee8d9f68ddcec34569c1330
Douglas DeVicariis <kootg04@gmail.com>
2015-09-30 14:48:45 -0700

    Handle delete post

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -8,6 +8,11 @@ module.exports = {
     feed("http://forum.solarmada.com/category/1.rss", function(err, articals) {
       if(err) return res.json({ message: err });
       
+      for(var i=0; i<articals.length; i++) {
+        if(articals[i].title == '[[topic:topic_is_deleted]]')
+          articals.splice(i);
+      }
+      
       data.news = articals;
       
       return res.view("homepage", { data: data, user: user });
