508e6e3389466c34f851838a992ff10f2084f625
Koo <kootg04@gmail.com>
2015-10-01 18:43:42 -0700

    Calendar Sorting

--- api/controllers/CalendarController.js
+++ api/controllers/CalendarController.js
@@ -11,9 +11,19 @@ module.exports = {
       if(err) return res.json({ message: err });
       
       for(var i=0; i<events.length; i++) {
+        var eDate = events[i].title.match(/\[(.*?)\]/);
+        var eDateSplit = eDate[1].split('&#x2F;');
+        var date = new Date(parseInt("20" + eDateSplit[2]), parseInt(eDateSplit[0])-1, parseInt(eDateSplit[1]));
+        events[i].date = date;
+        
         if(events[i].title == '[[topic:topic_is_deleted]]')
           events.splice(i);
       }
+      
+      events.sort(function(a,b) {
+        console.info(a.date);
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      });
       
       data.events = events;
       
