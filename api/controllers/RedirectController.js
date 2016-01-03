da1700d4106ade3bb34333824e61f676cd1272fd
Koo <kootg04@gmail.com>
2016-01-03 00:00:38 -0800

    fix

--- api/controllers/RedirectController.js
+++ api/controllers/RedirectController.js
@@ -1,5 +1,5 @@
 module.exports = {
     discord: function(req, res) {
-        return res.redirect('htts://discord.gg/0Vr0BMdhh6uB4isw');
+        return res.redirect('https://discord.gg/0Vr0BMdhh6uB4isw');
     }
 }
