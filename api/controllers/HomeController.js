c3e1f8ed1a163bfa8821ba7fd3e8a7e4a54e5968
Douglas DeVicariis <kootg04@gmail.com>
2016-05-02 11:03:26 -0700

    Quick Fix

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -10,7 +10,7 @@ module.exports = {
       data.discord = discord;
       feed("http://forum.solarmada.com/category/1.rss", function(err, articals) {
         if(err) {sails.log.error("Error When getting Announcements",err); return res.json({ message: err });}
-
+          sails.log.silly("Articals", articals);
           data.news = articals;
 
           return res.view("homepage", { data: data });
